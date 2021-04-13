79e826f2d826a67a60b54ef4c521e17aa0150f57
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2021-04-13 15:20:19 -0300

    install plugin and transform .html in .ejs

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const routes = express.Router();
-const basePath = __dirname + "/views";
 
-// resquest, reponse 
-routes.get('/', (req, res) => res.sendFile(basePath + "/index.html"));
-routes.get('/job', (req, res) => res.sendFile(basePath + "/job.html"));
-routes.get('/job-edit', (req, res) => res.sendFile(basePath + "/job-edit.html"));
-routes.get('/profile', (req, res) => res.sendFile(basePath + "/profile.html"));
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Arcanjo",
+    avatar: "https://avatars.githubusercontent.com/u/63671410?v=4",
+    "monthly-budget": 3000,
+    "hours-per-day": 5,
+    "days-per-week": 6,
+    "vocation-per-year": 4
+}
+
+routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/job', (req, res) => res.render(views + "job"));
+routes.get('/job-edit', (req, res) => res.render(views + "job-edit"));
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
 module.exports = routes;
